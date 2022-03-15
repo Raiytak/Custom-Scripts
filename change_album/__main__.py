138c4d0629e6fe2e3e38773621e533f157edda6b
Raiytak <mathieu.salaun12@gmail.com>
2022-03-15 12:26:33 +0100

    Script improvement

--- change_album/__main__.py
+++ change_album/__main__.py
@@ -1,31 +1,40 @@
+from typing import Union
 from pathlib import Path
 import eyed3
 import argparse
 
 MP3_EXTENSION = ".mp3"
+STRANGE_FILES = []
 
 
-def introduction():
-    print(
-        "To modify your .mp3 files, the path of the DIRECTORY \n",
-        "that contains the .mp3, and the is needed and\n",
-        "NAME of the album that you want to give \n",
-    )
-
-
-def changeAllFilesOfDirectoryRecursively(directory_path: Path):
+def changeAllFilesOfDirectoryRecursively(
+    directory_path: Path,
+    album_name: Union[str, None],
+    extension: str,
+    endpoint_only: bool,
+):
     unmodified_files = []
     list_files = directory_path.iterdir()
     for curr_file in list_files:
-        if curr_file.is_file():
-            unmodified = changeAlbumTagOfFile(curr_file, directory_path.stem)
+        modifications_allowed = (
+            checkIsEndpoint(directory_path, extension) if endpoint_only else True
+        )
+        if curr_file.is_file() and curr_file.suffix == extension:
+            unmodified = []
+            if modifications_allowed:
+                unmodified = changeAlbumTagOfFile(curr_file, album_name)
         else:
-            unmodified = changeAllFilesOfDirectoryRecursively(curr_file)
+            try:
+                unmodified = changeAllFilesOfDirectoryRecursively(
+                    curr_file, album_name, extension, endpoint_only
+                )
+            except NotADirectoryError:
+                STRANGE_FILES.append([directory_path, curr_file])
         unmodified_files += unmodified
     return unmodified_files
 
 
-def changeAlbumTagOfFile(path_file: Path, album_name: str):
+def changeAlbumTagOfFile(path_file: Path, album_name: Union[str, None]):
     audiofile = eyed3.load(str(path_file))
     if audiofile is None:
         print(
@@ -35,23 +44,28 @@ def changeAlbumTagOfFile(path_file: Path, album_name: str):
         )
         return [str(path_file)]
 
+    if album_name is None:
+        album_name = str(path_file.parent.stem)
     audiofile.tag.album = album_name
     audiofile.tag.save()
     print("Modified '{}' album's name to '{}'".format(path_file.stem, album_name))
     return []
 
 
-def getAllFileWithExtension(folder_path: Path, extension: str) -> list:
-    return [x for x in folder_path.iterdir() if x.is_file() and x.suffix == extension]
-
-
-def askPathDirectory(needs_to_be_endpoint: bool) -> Path:
+def askPathDirectory(endpoint_only: bool, recursively: bool, extension: str) -> Path:
     is_good_dir = False
     while not is_good_dir:
         path_dir = Path(str(input("Directory path (none=realtive) = ")))
         if path_dir.is_dir():
-            if needs_to_be_endpoint:
-                is_good_dir = checkIsEndpoint(path_dir, MP3_EXTENSION)
+            if not recursively:
+                if endpoint_only:
+                    is_good_dir = checkIsEndpoint(path_dir, extension)
+                    if not is_good_dir:
+                        print(
+                            "The path provided does not contains only {} files".format(
+                                extension
+                            )
+                        )
             else:
                 is_good_dir = True
         else:
@@ -60,12 +74,10 @@ def askPathDirectory(needs_to_be_endpoint: bool) -> Path:
 
 
 def checkIsEndpoint(folder_path: Path, extension: str):
-    list_files = getAllFileWithExtension(folder_path, extension)
-    # Assert that all files within the directory are of the expected extension
-    if all([file.stem == extension for file in list_files]):
+    """Returns True if all files within the directory are of the expected extension"""
+    list_files = folder_path.iterdir()
+    if all([file.suffix == extension for file in list_files]):
         return True
-    else:
-        print(f"The directory given does not contains {extension} file")
     return False
 
 
@@ -79,8 +91,6 @@ def askAlbumName():
 
 
 if __name__ == "__main__":
-    introduction()
-
     parser = argparse.ArgumentParser(
         description="Alterate album's name of '.mp3' files."
     )
@@ -91,37 +101,68 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument(
-        "--all",
-        help="If set, modifies recursively all the '.mp3' files for the given path, set every file's album's name to their directory's name",
-        nargs="?",
+        "-n",
+        "--album-name",
+        help="Path to the directory containing the '.mp3' files to modify",
+        type=str,
     )
     parser.add_argument(
-        "--endpoints-only",
-        help="Modifies the folders contaigning only '.mp3' files",
-        nargs="?",
+        "--recursively",
+        help="If set, modifies recursively all the '.mp3' files for the given path",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--endpoint-only",
+        help="Modifies folders contaigning only '.mp3' files",
+        action="store_true",
     )
     args = parser.parse_args()
 
-    needs_to_be_endpoint = args.endpoints_only is not None
+    endpoint_only = args.endpoint_only
     if not args.path:
-        user_path = askPathDirectory(needs_to_be_endpoint)
+        user_path = askPathDirectory(endpoint_only, args.recursively, MP3_EXTENSION)
     else:
         user_path = Path(args.path)
-        if needs_to_be_endpoint:
-            checkIsEndpoint(user_path, MP3_EXTENSION)
 
-    if args.all is None:
-        album_name = askAlbumName()
-        list_files = getAllFileWithExtension(user_path, MP3_EXTENSION)
-        for name_mp3 in list_files:
-            unmodified_files = changeAlbumTagOfFile(name_mp3, album_name)
+    album_name = None
+    if args.album_name:
+        album_name = args.album_name
     else:
-        unmodified_files = changeAllFilesOfDirectoryRecursively(user_path)
+        if not args.recursively:
+            album_name = askAlbumName()
+
+    if not args.recursively:
+        if endpoint_only:
+            is_ok = checkIsEndpoint(user_path, MP3_EXTENSION)
+            if not is_ok:
+                raise AttributeError(
+                    "The given directory does not contains only {} files".format(
+                        MP3_EXTENSION
+                    )
+                )
+        list_files = [
+            x for x in user_path.iterdir() if x.is_file() and x.suffix == MP3_EXTENSION
+        ]
+        for file in list_files:
+            unmodified_files = changeAlbumTagOfFile(file, album_name)
+    else:
+        unmodified_files = changeAllFilesOfDirectoryRecursively(
+            user_path, album_name, MP3_EXTENSION, endpoint_only
+        )
     if unmodified_files != []:
         print("\n## The following files could not be loaded using eyed3: ##")
         print("\n".join(unmodified_files))
 
+    if STRANGE_FILES != []:
+        print(
+            "\n".join(
+                [
+                    "**A strange file has been detected inside '{}' named '{}'**".format(
+                        directory_path, curr_file.stem
+                    )
+                    for directory_path, curr_file in STRANGE_FILES
+                ]
+            )
+        )
+
     print("The operation is a success!")
-
-
-# /home/mathieu/Music/Anime
